cb247ee93e060f416b7b815ca58f73c29b1e4c6c
facuno Moreno <1913100113@untels.edu.pe>
2024-07-03 12:11:33 -0500

    Avance Semana 14

---
+++ app/src/main/java/com/example/dam_sem11_proyecto/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dam_sem11_proyecto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Definir el evento click sobre el botón Login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasAtivity.class);
+                startActivity(i);
+            }
+
+        });
+
+    }
+}
